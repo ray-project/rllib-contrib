bcc45e135eb03f3498397b79bb9e468cb8a04074
Avnish <avnishnarayan@gmail.com>
2023-04-19 16:31:18 -0700

    Move maml to its own folder
    Signed-off-by: Avnish <avnishnarayan@gmail.com>

--- MAML/src/rllib_maml/maml/maml_tf_policy.py
+++ MAML/src/rllib_maml/maml/maml_tf_policy.py
@@ -153,7 +153,6 @@ class MAMLLoss(object):
         vf_loss_coeff=1.0,
         use_gae=True,
     ):
-
         self.config = config
         self.num_tasks = num_tasks
         self.inner_adaptation_steps = inner_adaptation_steps
